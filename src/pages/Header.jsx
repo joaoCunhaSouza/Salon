f1c81bbc6c1a40119213be0da9df8578e2acd41f
JOÃO VICTOR CUNHA DE SOUZA <joao-victor_c_souza@estudante.sesisenai.org.br>
2024-10-10 19:53:41 -0300

    Upload

---
+++ src/pages/Header.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+import '../Header.css'; 
+
+export function Header() {
+
+  return (
+    <header className="header">
+      <div className="hero">
+        <h1 className="Logo">Jaque’s</h1>
+      </div>
+      <nav>
+        <ul className="ul">
+          <li><Link to="/"> Home</Link></li>
+          <li><Link to="/Features"> About</Link></li>
+          <li><Link to="/Community"> Contact</Link></li>
+          
+          div
+          <li className="no-list-style-0"><Link to="/Register" className="more-link-0">Sing in</Link></li>
+        </ul>
+      </nav>
+    </header>
+  )
+
+}
+
+
