086488885447241efe86711151fdf426d110c56d
joao Stempniak <joao48991884694@gmail.com>
2024-10-20 19:52:04 -0300

    n mexa

---
+++ src/pages/Login.jsx
@@ -0,0 +1,23 @@
+import Container from '../container.jsx';
+
+export function Login() {
+
+
+    return (
+        <>
+        
+
+        <h1>Login de sistema</h1>
+        <input type="email" name="" id="" />
+        <input type="password" name="" id="" />
+
+
+
+    
+        
+       
+        
+        </>
+    )
+  }
+ 
