a4dfa97ca227eb9ab19322a19557ff298b698438
JOÃO VICTOR CUNHA DE SOUZA <joao-victor_c_souza@estudante.sesisenai.org.br>
2024-10-23 19:58:16 -0300

    update01-23/10/2024

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,33 +1,33 @@
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faStar } from '@fortawesome/free-solid-svg-icons'; // Importando o ícone da estrela
 import '../Header.css'; 
 
 export function Header() {
   return (
     <header className="header">
       <div className="hero">
+        <div className="linha">
         <h1 className="Logo">Jaque’s</h1>
+        <strong className="ston">5.0</strong>
+        <FontAwesomeIcon icon={faStar} /> {/* Usando o ícone da estrela corretamente */}
+      </div>
       </div>
       <nav>
         <ul className="ul">
-
           <div className="arrumar">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/Features">About</Link></li>
-          <li><Link to="/Community">Contact</Link></li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/Features">About</Link></li>
+            <li><Link to="/Community">Contact</Link></li>
           </div>
 
           <div className="login">
-          <li className="no-list-style-0">
-            <Link to="/Register" className="more-link-0">Sign in</Link>
-       
-            <FontAwesomeIcon icon={faUser} />
-          
-          </li>
-        </div>
+            <li className="no-list-style-0">
+              <FontAwesomeIcon icon={faUser} />
+              <Link to="/Register" className="more-link-0">Sign in</Link>
+            </li>
+          </div>
         </ul>
-
       </nav>
     </header>
   );
